796dd6d325e4eb0e0fe10ac9fb2dc5eb5b5906ba
Jesse Rosenberger <git@jro.cc>
2018-09-14 17:00:42 +0300

    Cleanup `engine` option and remove trailing spaces.
    The `ApolloServer` constructor options allow `engine` to be set to
    `false`.  While the alternative of setting it to `undefined` (as it was
    before) does effectively work the same, having the explicit `undefined` in
    there gave the impression that `undefined` might be a desired attribute for
    the `engine` option.  Plus, this was a bit more concise.
    
    Also, removed some trailing spaces.  All in all this was mostly nitpicks.

--- index.js
+++ index.js
@@ -43,12 +43,12 @@
   // In the most basic sense, the ApolloServer can be started
   // by passing type definitions (typeDefs) and the resolvers
   // responsible for fetching the data for those types.
-  const server = new ApolloServer({ 
-    typeDefs, 
+  const server = new ApolloServer({
+    typeDefs,
     resolvers,
-    engine: process.env.ENGINE_API_KEY ? {
-      apiKey: process.env.ENGINE_API_KEY
-    } : undefined
+    engine: process.env.ENGINE_API_KEY && {
+      apiKey: process.env.ENGINE_API_KEY,
+    },
   });
 
   // This `listen` method launches a web-server.  Existing apps
