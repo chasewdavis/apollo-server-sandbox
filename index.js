098fcc57e0a2bb9ab040797a5d2ecdf890b7b701
Chase Davis <cdavis@cdaviss-MacBook-Pro.local>
2019-01-05 16:08:28 -0800

    formatting

--- index.js
+++ index.js
@@ -1,69 +1,69 @@
-  const { ApolloServer, gql, PubSub } = require('apollo-server');
+const { ApolloServer, gql, PubSub } = require('apollo-server');
 
-  const pubsub = new PubSub();
-  const BOOK_ADDED = 'BOOK_ADDED';
+const pubsub = new PubSub();
+const BOOK_ADDED = 'BOOK_ADDED';
 
-  const books = [
+const books = [
     {
-      title: 'Harry Potter and the Chamber of Secrets',
-      author: 'J.K. Rowling',
+        title: 'Harry Potter and the Chamber of Secrets',
+        author: 'J.K. Rowling'
     },
     {
-      title: 'Jurassic Park',
-      author: 'Michael Crichton',
-    },
-  ];
+        title: 'Jurassic Park',
+        author: 'Michael Crichton'
+    }
+];
 
-  const typeDefs = gql`
+
+const typeDefs = gql`
     type Book {
-      title: String
-      author: String
+        title: String
+        author: String
     }
 
     type Query {
-      books (limit:Int): [Book]
+        books (limit:Int): [Book]
     }
 
     type Mutation {
-      addBook(title: String!, author: String!): Book 
+        addBook(title: String!, author: String!): Book 
     }
 
     type Subscription {
-      bookAdded: Book
+        bookAdded: Book
     }
   `;
 
-  const resolvers = {
+// comments
+const resolvers = {
     Query: {
-      books: (_, { limit }, context) => { 
-        return limit || limit === 0 ?  books.slice(0, limit) : books;
-      },
+        books: (_, { limit }, context) => (limit || limit === 0 ? books.slice(0, limit) : books)
     },
 
     Mutation: {
-      addBook: (_, { title, author }, context) => {
-        const book = { title, author };
-        books.push(book);
-        pubsub.publish(BOOK_ADDED, { bookAdded: book });
-        return book;
-      }
+        addBook: (_, { title, author }, context) => {
+            const book = { title, author };
+            books.push(book);
+            pubsub.publish(BOOK_ADDED, { bookAdded: book });
+            return book;
+        }
     },
 
     Subscription: {
-      bookAdded: {
-        subscribe: () => pubsub.asyncIterator([BOOK_ADDED])
-      } 
+        bookAdded: {
+            subscribe: () => pubsub.asyncIterator([BOOK_ADDED])
+        }
     }
-  };
+};
 
-  const server = new ApolloServer({
+const server = new ApolloServer({
     typeDefs,
     resolvers,
     engine: process.env.ENGINE_API_KEY && {
-      apiKey: process.env.ENGINE_API_KEY,
-    },
-  });
+        apiKey: process.env.ENGINE_API_KEY
+    }
+});
 
-  server.listen().then(({ url }) => {
+server.listen().then(({ url }) => {
     console.log(`🚀  Server ready at ${url}`);
-  });
+});
