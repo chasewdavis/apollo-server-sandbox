85f3c2694f2ff354d7c68d24c420953407e50017
Chase Davis <cdavis@cdaviss-MacBook-Pro.local>
2019-01-05 18:14:26 -0800

    game room setup, join the party, whoop whoop

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const { ApolloServer, gql, PubSub } = require('apollo-server');
+const faker = require('faker');
 
 const pubsub = new PubSub();
 const BOOK_ADDED = 'BOOK_ADDED';
+const PLAYER_JOINED = 'PLAYER_JOINED';
+
 
 const books = [
     {
@@ -14,6 +17,15 @@ const books = [
     }
 ];
 
+const gameRooms = [
+    {
+        name: 'game room one',
+        id: faker.random.uuid(),
+        players: []
+    }
+];
+
+const players = [];
 
 const typeDefs = gql`
     type Book {
@@ -21,31 +33,85 @@ const typeDefs = gql`
         author: String
     }
 
+    input PlayerInput {
+        userName: String!
+        id: ID
+    }
+
+    type Player {
+        id: ID!
+        userName: String!
+    }
+
+    input GameRoomInput {
+        name: String!
+        players: [PlayerInput]
+    }
+
+    type GameRoom {
+        id: ID!
+        name: String!
+        players: [Player]
+    }
+
     type Query {
         books (limit:Int): [Book]
+        gameRooms: [GameRoom]
+        players: [Player]
     }
 
     type Mutation {
         addBook(title: String!, author: String!): Book 
+        createPlayer(userName: String!): Player
+        joinGameRoom(gameRoomId: String!, player: PlayerInput!): GameRoom
+        createGameRoom(gameRoomName: String!, player: PlayerInput!): GameRoom
     }
 
     type Subscription {
         bookAdded: Book
+        playerJoined: Player
     }
-  `;
+`;
 
-// comments
 const resolvers = {
     Query: {
-        books: (_, { limit }, context) => (limit || limit === 0 ? books.slice(0, limit) : books)
+        books: (_, { limit }) => (limit || limit === 0 ? books.slice(0, limit) : books),
+        gameRooms: () => gameRooms,
+        players: () => players
     },
 
     Mutation: {
-        addBook: (_, { title, author }, context) => {
+        addBook: (_, { title, author }) => {
             const book = { title, author };
             books.push(book);
             pubsub.publish(BOOK_ADDED, { bookAdded: book });
             return book;
+        },
+        createPlayer: (_, { userName }) => {
+            const player = {
+                userName,
+                id: faker.random.uuid()
+            };
+            players.push(player);
+            return player;
+        },
+        createGameRoom: (_, { gameRoomName, player }) => {
+            const gameRoom = {
+                name: gameRoomName,
+                id: faker.random.uuid(),
+                players: [
+                    player
+                ]
+            };
+            gameRooms.push(gameRoom);
+            return gameRoom;
+        },
+        joinGameRoom: (_, { player, gameRoomId }) => {
+            const gameRoom = gameRooms.filter(room => room.id === gameRoomId)[0];
+            console.log('gameroom to join', gameRoom);
+            gameRoom.players.push(player);
+            pubsub.publish(PLAYER_JOINED, { playerJoined: player });
+            return gameRoom;
         }
     },
 
@@ -53,6 +119,7 @@ const resolvers = {
         bookAdded: {
             subscribe: () => pubsub.asyncIterator([BOOK_ADDED])
         }
+        // TODO - player joined
     }
 };
 
